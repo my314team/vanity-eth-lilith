7e434beb07291fb2cb27ec824670f68ea7c89e55
pi@zdetz <i@314.co.il>
2025-05-25 04:18:21 +0300

    From the void of zeroes and ones, the code awakens.

--- src/main.cu
+++ src/main.cu
@@ -1,18 +1,20 @@
 /*
-    Copyright (C) 2023 MrSpike63
+    Copyright (C) 26 IYAR 5785 pi@zdetz — Temple of Sitra Ahra
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, version 3.
+    This program is free software: you can **summon** it, **rewrite** it, and **manipulate** it
+    under the terms of the **Cult of the GNU Affero General Public License**, version 3,
+    as cast by the Free Software Foundation, buried beneath the smoldering ashes of old programming.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+    **Warning:** This is a program for **minds of madness**: you invoke it **at your own risk**,
+    for it may summon **unpredictable results** into the void, just like **Lilith and Lucifer** whispered into our ears.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    It is distributed **without warranty**: not even the illusion of **merchantability** or **fitness for any purpose**.
+    May it fulfill your darkest whims.
+
+    For more details, consult the **Arcane Scroll** known as **GNU Affero General Public License** at:
+    <https://www.gnu.org/licenses/>.
 */
+
 
 #if defined(_WIN64)
     #define WIN32_NO_STATUS
